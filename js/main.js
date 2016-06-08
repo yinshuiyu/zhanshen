8a66d4becb23595f981e959a167b4594243ed364
kangning <liu.kangning@163.com>
2016-06-08 16:35:39 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    // FastClick.attach(document.body);
+    /*
+    //禁止橡皮筋
+	function stopScrolling( touchEvent ) { 
+		touchEvent.preventDefault(); 
+	} 
+	document.addEventListener( 'touchstart' , stopScrolling , false ); 
+	document.addEventListener( 'touchmove' , stopScrolling , false );
+	*/
+})
